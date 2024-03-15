f82f3515d4e43ce8d9c89325ec0ea3cb5a02766c
Purushottam620xyz <156118354+Purushottam620xyz@users.noreply.github.com>
2024-03-15 17:25:21 +0530

    Add files via upload

---
+++ project1.js
@@ -0,0 +1,7 @@
+let gamenum=25;
+let usernum=prompt("guess the number :");
+while(usernum!=gamenum){
+usernum=prompt("you entered the wrong number guess again :");
+// prompt always store number in string
+}
+console.log("congratulations ,you entered the right number");
